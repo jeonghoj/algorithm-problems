c05698bb94c71ee6133b2fe588f6be9f988cad02
jeonghoj <jeonghoj94@gmail.com>
2020-01-15 23:53:43 +0900

    refactoring

--- python/leetcode/11.py
+++ python/leetcode/11.py
@@ -1,19 +1,24 @@
 # height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 height = [1, 2, 1]
-max_area_list = []
-
+# 두개의 포인터를 이용
 start_pointer = 0
 last_pointer = len(height) - 1
 
-while start_pointer != last_pointer:
-    if height[start_pointer] >= height[last_pointer]:
-        print(height[last_pointer], (abs(start_pointer - last_pointer)))
-        max_area_list.append(height[last_pointer] * (abs(start_pointer - last_pointer)))
-        last_pointer -= 1
+max_area = 0
+
+# 두개의 포인터가 교차할때까지
+while start_pointer > last_pointer:
+    # 높이가 낮은것 * 포인터 사이의 거리
+    max(max_area, min(height[start_pointer], height[last_pointer]) * abs(start_pointer - last_pointer))
+
+    # 포인터 이동
+
+    # 포인터가 이동할 시, x가 줄어드는데에 따른 y 길이를 최대화 하기 위해,
+    # y가 최대인 쪽을 유지하여 너비 최대화
+    # 높이가 낮은 쪽의 포인터를 이동
+    if height[start_pointer] > height[last_pointer]:
+        last_pointer += 1
     else:
-        print(height[last_pointer], (abs(start_pointer - last_pointer)))
-        max_area_list.append(height[start_pointer] * (abs(start_pointer - last_pointer)))
         start_pointer += 1
 
-print(max_area_list)
-print(max(max_area_list))
+
